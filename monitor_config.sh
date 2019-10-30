55f66398fceecabe9bfe53abd76279f2bdbab76c
ezreker <ezekiel.rosenbluth@gmail.com>
2019-10-30 15:02:20 -0400

    Delete monitor_config.sh

--- monitor_config.sh
+++
@@ -1,6 +0,0 @@
-#be sure to modify paths in plist and monitor programbrew install python3
-
-pip3 install watchdog
-
-sudo chown root /Library/LaunchDaemons/com.zeke.monitor.plist
-sudo chgrp wheel /Library/LaunchDaemons/com.zeke.monitor.plist
